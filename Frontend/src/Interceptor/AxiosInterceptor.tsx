edee83a4ab78e0bebceb6d98fbb2b44174edcd67
naitikvj31 <99116285+naitikvj31@users.noreply.github.com>
2025-05-09 22:06:53 +0530

    Update AxiosInterceptor.tsx

--- Frontend/src/Interceptor/AxiosInterceptor.tsx
+++ Frontend/src/Interceptor/AxiosInterceptor.tsx
@@ -2,8 +2,13 @@ import axios, { InternalAxiosRequestConfig } from "axios";
 import { removeUser } from "../Slices/UserSlice";
 import { removeJwt } from "../Slices/JwtSlice";
 
+// Use environment variable for base URL
+const baseURL = process.env.NODE_ENV === 'production' 
+    ? 'https://arbitar-backend.onrender.com' 
+    : 'http://localhost:8080';
+
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: baseURL
 });
 
 axiosInstance.interceptors.request.use(
@@ -26,7 +31,6 @@ export const setupResponseInterceptor = (navigate: any, dispatch: any) => {
         },
         (error) => {
             if (error.response?.status === 401) {
-
                 dispatch(removeUser());
                 dispatch(removeJwt());
                 navigate('/login');
@@ -35,5 +39,49 @@ export const setupResponseInterceptor = (navigate: any, dispatch: any) => {
         }
     )
 }
+
+export default axiosInstance;
+
+
+
+
+
+// import axios, { InternalAxiosRequestConfig } from "axios";
+// import { removeUser } from "../Slices/UserSlice";
+// import { removeJwt } from "../Slices/JwtSlice";
+
+// const axiosInstance = axios.create({
+//     baseURL: 'http://localhost:8080'
+// });
+
+// axiosInstance.interceptors.request.use(
+//     (config: InternalAxiosRequestConfig) => {
+//         const token = localStorage.getItem('token');
+//         if (token) {
+//             config.headers.Authorization = `Bearer ${token}`;
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// )
+
+// export const setupResponseInterceptor = (navigate: any, dispatch: any) => {
+//     axiosInstance.interceptors.response.use(
+//         (response) => {
+//             return response;
+//         },
+//         (error) => {
+//             if (error.response?.status === 401) {
+
+//                 dispatch(removeUser());
+//                 dispatch(removeJwt());
+//                 navigate('/login');
+//             }
+//             return Promise.reject(error);
+//         }
+//     )
+// }
 
 export default axiosInstance;
